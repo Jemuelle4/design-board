05be49f1c73283fe06a9b14de5e1852416b805f2
Sam <samjquiambao@gmail.com>
2024-05-20 00:08:10 -0700

    feat: :art: Changed words in html and added another function to show footer

--- template.js
+++ template.js
@@ -17,4 +17,22 @@ function showNavBar(){
   })
 }
 
+function showFooter(){
+  const footerTemplatePath = 'templates/footer.html';
+
+  fetch(footerTemplatePath)
+    .then(response => response.text())
+    .then(templateContent =>{
+      const tempDiv = document.createElement('div');
+      tempDiv.innerHTML = templateContent;
+      const template = tempDiv.querySelector('#footer-template');
+
+      const cloneTemplateContent = document.importNode(template.content, true);
+
+      document.getElementById('footer-container').appendChild(cloneTemplateContent);
+    })
+    .catch(error => console.error('Error loading the template:', error));
+}
+
 showNavBar()
+showFooter()
